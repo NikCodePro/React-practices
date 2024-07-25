83012c03567c52a71e14f5f1e8a23bce2a80d287
NikCodePro <vermanikhil7838@gmail.com>
2024-07-25 18:42:29 +0530

    api projecet

---
+++ api-card-project/src/App.jsx
@@ -0,0 +1,21 @@
+import Cards from './components/Cards'
+
+import './App.css'
+
+function App() {
+
+  // let obj = {
+  //   "name": "nikk",
+  //   "class": "finaly",
+  //   "sub": "mern",
+  //   "proof": "py",
+  // }
+
+  return (
+    <>
+    <Cards/>
+    </>
+  )
+}
+
+export default App
