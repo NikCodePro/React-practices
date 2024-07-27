4b2c1fe5af460db210805faeecb51ef29f8ac814
NikCodePro <vermanikhil7838@gmail.com>
2024-07-27 17:18:05 +0530

    project pending

---
+++ passowr-generator/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container w-screen h-screen flex flex-wrap justify-center align-middle">
+        <div className="border border-emerald-200">
+          <h1 className="text-2xl font-bold">Password Generator</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
